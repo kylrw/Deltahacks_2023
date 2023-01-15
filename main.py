fd3822e9ae250ad845b9f852f9ad3d7c9d943979
kylrw <kwitvoet03@gmail.com>
2023-01-14 21:56:05 -0500

    webscraper function in main

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
 import os
 import openai
 import AccessKeys
+import requests
+from bs4 import BeautifulSoup
+
+def extract_article(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Extract the article content
+    article_content = soup.find("div", {"class": "c-entry-content"})
+    if article_content is None:
+        article_content = soup.find("div", {"class": "gnt_ar_b"})
+    if article_content is None:
+        article_content = soup.find("div", {"class": "article-content"})
+    if article_content is None:
+        article_content = soup.find("div", {"class": "entry-content"})
+    if article_content is None:
+        article_content = soup.find("div", {"class": "article-text"})
+    if article_content is None:
+        article_content = soup.find("div", {"class": "post-content"})
+    if article_content is None:
+        article_content = soup.find("div", {"class": "content"})
+    if article_content is None:
+        article_content = soup.find("div", {"class": "entry"})
+    if article_content is None:
+        article_content = soup.find("div", {"class": "article"})
+    if article_content is None:
+        article_content = soup.find("div", {"class": "main-content"})
+    if article_content is None:
+        print("Can't find the article content, please check the class name")
+        return None
+    # Print the article content
+    return article_content.text
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
